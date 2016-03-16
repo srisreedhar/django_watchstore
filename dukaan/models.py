75c2e5279d78955d44dcbdd26a4a84e174987607
name <srisreedhar@live.com>
2016-03-16 12:36:19 +0530

    created model for Watch

--- dukaan/models.py
+++ dukaan/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# creating a model for Watch
+
+class Watch(models.Model):
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=30)
+    description = models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(decimal_places=2,max_digits=9)
+    stock = models.IntegerField(default=0)
+
+
+
